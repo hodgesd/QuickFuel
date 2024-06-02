a7a2938722e33f7f5fed6e35898a2e7aa194b0f1
hodgesd <hodgesd@gmail.com>
2024-06-02 11:15:45 -0500

    Update project name and enhance app functionality
    The changes made include updating the project name from "Fuel Calculator" to "GVII Fuel Calculator". Functionality of the app has been enhanced by including 'fuel_presets' array and adding buttons for the same. Visibility of some elements have also been altered based on the state of 'change_visibility' function.

--- src/helloworld/app.py
+++ src/helloworld/app.py
@@ -1,17 +1,20 @@
 """
 My first Toga application
 """
+import sys
 
 import toga
 from toga.style import Pack
 from toga.style.pack import COLUMN, ROW, CENTER, MONOSPACE, BOLD, HIDDEN
 
+print("executable:", sys.executable)
 
 class HelloWorld(toga.App):
 
     def startup(self):
         BLUE = "#006db6"
         self.accent_color = BLUE
+        fuel_presets = [10000, 15000, 20000, 30000, 41500]
 
         main_box = toga.Box(style=Pack(direction=COLUMN, alignment=CENTER, padding=20))
         self.fuel_volume_label = "gals"
@@ -22,14 +25,18 @@ class HelloWorld(toga.App):
         # self.name_input = toga.NumberInput(style=Pack(visibility=HIDDEN,flex=1))
         # self.name_input.on_confirm = self.say_hello
 
+        self.starting_title = toga.Label("Gauge", style=Pack(visibility=HIDDEN, padding_top=(35), text_align=CENTER,
+                                                           color=self.accent_color, font_family=MONOSPACE,
+                                                           font_size=24, font_weight=BOLD))
         self.starting_slider = toga.Slider(min=4000, max=15000, tick_count=111, value=5000,on_change=self.on_starting_slider_change,
-                                    style=Pack(visibility=HIDDEN,color=self.accent_color,padding=10,flex=1))
+                                    style=Pack(visibility=HIDDEN,color=self.accent_color,flex=1))
         self.starting_label_lbs = toga.Label(
             f"{self.starting_slider.value:,.0f} lbs",
             style=Pack(visibility=HIDDEN, padding=(0, 5), text_align=CENTER, font_family=MONOSPACE,
                        font_size=24, font_weight=BOLD,color=self.accent_color))
 
         self.starting_box = toga.Box(style=Pack(direction=COLUMN, padding=(0,15)))
+        self.starting_box.add(self.starting_title)
         self.starting_box.add(self.starting_slider)
         self.starting_box.add(self.starting_label_lbs)
 
@@ -49,7 +56,9 @@ class HelloWorld(toga.App):
 
         self.flex_box = toga.Box(style=Pack(flex=1))
 
-
+        self.fuel_slider_title = toga.Label("Conversion", style=Pack(text_align=CENTER,
+                                                           font_family=MONOSPACE,
+                                                           font_size=24, font_weight=BOLD))
         self.fuel_slider = toga.Slider(min=5000, max=41500, tick_count=366, on_change=self.on_fuel_slider_change,
                                        on_release=self.on_fuel_slider_release, style=Pack(flex=1))
         self.fuel_label_volume = toga.Label(f"{self.fuel_slider.value / self.CONVERSION_FACTOR:,.0f} {self.fuel_volume_label}",
@@ -58,27 +67,32 @@ class HelloWorld(toga.App):
         self.fuel_label_weight = toga.Label(f"{self.fuel_slider.value:,.0f} lbs",
                                          style=Pack(padding=(0, 5), text_align=CENTER, font_family=MONOSPACE,
                                                     font_size=24, font_weight=BOLD))
-        fuel_slider_box = toga.Box(style=Pack(direction=COLUMN, padding=25, alignment=CENTER))
+        fuel_slider_box = toga.Box(style=Pack(direction=COLUMN, padding=(60,25), alignment=CENTER))
+        # Create buttons using a loop
+        button_box = toga.Box(style=Pack(direction=ROW, padding=(0, 15)))
+        for pounds in fuel_presets:
+            label = "Max" if pounds == 41500 else f"{pounds // 1000}K"
+            button = toga.Button(label, on_press=lambda widget, p=pounds: self.set_pounds(widget, p),
+                                 style=Pack(padding=(5,15)))
+            button_box.add(button)
+
+        fuel_slider_box.add(self.fuel_slider_title)
+        fuel_slider_box.add(button_box)
         fuel_slider_box.add(self.fuel_label_weight)
         fuel_slider_box.add(self.fuel_slider)
         fuel_slider_box.add(self.fuel_label_volume)
 
-        self.delta_title = toga.Label("Uplift",style=Pack(visibility=HIDDEN, padding_top=(35), text_align=CENTER, color=self.accent_color, font_family=MONOSPACE,
+        self.delta_title = toga.Label("Uplift",style=Pack(visibility=HIDDEN, padding_top=(60), text_align=CENTER, color=self.accent_color, font_family=MONOSPACE,
                                                     font_size=24, font_weight=BOLD))
         self.delta_volume_label = toga.Label(f"{(self.fuel_slider.value - self.starting_volume_lbs ) / self.CONVERSION_FACTOR:,.0f} {self.fuel_volume_label}", style=Pack(visibility=HIDDEN,padding=(10), text_align=CENTER, color=self.accent_color, font_family=MONOSPACE,
                                                     font_size=24, font_weight=BOLD))
         fuel_slider_box.add(self.delta_title)
         fuel_slider_box.add(self.delta_volume_label)
 
-        fuel_amounts = [10000, 15000, 20000, 30000, 41500]
 
-        # Create buttons using a loop
-        button_box = toga.Box(style=Pack(direction=ROW, padding=15))
-        for pounds in fuel_amounts:
-            label = "Max" if pounds == 41500 else f"{pounds // 1000}K"
-            button = toga.Button(label, on_press=lambda widget, p=pounds: self.set_pounds(widget, p),
-                                 style=Pack(padding=5))
-            button_box.add(button)
+
+
+
 
 
         switch_box = toga.Box(style=Pack(direction=ROW, padding_bottom=20, flex=1))
@@ -87,7 +101,6 @@ class HelloWorld(toga.App):
         switch_box.add(self.box_is_liters_switch)
 
         main_box.add(self.starting_box)
-        main_box.add(button_box)
         main_box.add(fuel_slider_box)
 
         self.flex_box2 = toga.Box(style=Pack(flex=1))
@@ -95,7 +108,7 @@ class HelloWorld(toga.App):
         # Set the main window
         self.main_window = toga.MainWindow(title=self.formal_name)
         self.main_window.content = toga.Box(children=[main_box, self.flex_box2, switch_box],
-                                            style=Pack(direction=COLUMN, padding=0, )
+                                            style=Pack(direction=COLUMN, padding=(40,0) )
                                             )
 
         self.main_window.show()
@@ -113,6 +126,7 @@ class HelloWorld(toga.App):
         self.change_visibility(visibility)
 
     def change_visibility(self, visibility):
+        self.starting_title.style.visibility = visibility
         self.starting_slider.style.visibility = visibility
         self.starting_label_lbs.style.visibility = visibility
         self.delta_title.style.visibility = visibility
